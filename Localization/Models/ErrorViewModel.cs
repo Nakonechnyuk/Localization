ac83780b60326c140bc84aca3f1a27194981f182
Oleksandra Nakonechniuk <onakonec@softserveinc.com>
2023-07-07 12:37:54 +0300

    fix

--- Localization/Models/ErrorViewModel.cs
+++ Localization/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Localization.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
